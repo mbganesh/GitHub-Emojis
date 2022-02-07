de08f572c9eaaa5e03b98cdf5522b728626fd928
mbganesh <crazyganesh179@gmail.com>
2022-02-07 23:41:21 +0530

    v1.0 Emojies 😍

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { AppBar, Toolbar, Typography, TextField } from '@mui/material';
+import useStateRef from 'react-usestateref';
+import axios from 'axios'
 
 function App() {
+
+
+  const [myText, setMyText, myTextRef] = useStateRef('')
+
+  const [myPic, setMyPic, myPicRef] = useStateRef('https://github.githubassets.com/images/icons/emoji/unicode/1f1ec-1f1f7.png?v8')
+
+  const handleText = (e) => {
+    setMyText(e.target.value)
+    getResPic(myTextRef.current)
+  }
+
+
+ const  getResPic = text => {
+  axios.get('https://api.github.com/emojis').then(result => {
+      var json = result.data
+
+      var keys = Object.keys(json)
+
+      var isPresent =  keys.filter(el => el === myTextRef.current)
+
+      if(isPresent.length === 1){
+        setMyPic(json[myTextRef.current])
+      }
+    })
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div >
+      <AppBar position='fixed'>
+        <Toolbar>
+          <Typography style={{ fontSize: '25px' }} > GitHub Fun </Typography>
+        </Toolbar>
+      </AppBar>
+
+
+      <div style={{ width: '100vw', height: '100vh', backgroundColor: '#FBF8F1', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+
+
+        <div style={{ width: '450px', height: '450px', backgroundColor: '#B1D0E0', display: 'flex', flexDirection: 'column' }}>
+
+
+          <TextField style={{ margin:'20px'  }} label="Type Here " variant="outlined" value={myTextRef.current} onChange={handleText} />
+
+
+          <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', height: '100%' }}>
+            <img alt="loading" style={{ width: '250px', }} src={myPicRef.current} />
+          </div>
+
+
+        </div>
+      </div>
+
+
     </div>
   );
 }
